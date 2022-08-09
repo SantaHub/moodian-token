dcb4c39a6ed689ebaff2a6c8f3de0a5c759e5c76
Austin P Blaise <pbaustin96@gmail.com>
2022-08-09 11:10:23 -0400

    updating token class in deploy script

--- scripts/1_deploy_token.py
+++ scripts/1_deploy_token.py
@@ -1,4 +1,4 @@
-from brownie import OurToken
+from brownie import MDNToken
 from scripts.helpful_scripts import get_account
 from web3 import Web3
 
@@ -6,5 +6,5 @@ initial_supply = Web3.toWei(1000, "ether")
 
 def main():
     account = get_account()
-    our_token = OurToken.deploy(initial_supply, {"from": account})
+    our_token = MDNToken.deploy(initial_supply, {"from": account})
     print(our_token.name())
