0c7a2218078a15fa9dd7ed65bfa9f71c7c6bb681
Austin P Blaise <pbaustin96@gmail.com>
2022-08-19 09:15:39 -0400

    adding transfer fn

--- scripts/1_deploy_token.py
+++ scripts/1_deploy_token.py
@@ -4,15 +4,19 @@ from brownie.network.gas.strategies import LinearScalingStrategy
 from scripts.helpful_scripts import get_account
 from web3 import Web3
 
-
 initial_supply = Web3.toWei(1000, "ether")
 
 gas_strategy = LinearScalingStrategy("60 gwei", "70 gwei", 1.1)
 
-gas_price(gas_strategy)
-
 
 def main():
-    account = get_account()
-    mdk_token = MDNToken.deploy(initial_supply, {"from": account, "gas_price": gas_strategy})
+    account_1 = get_account()
+    print(account_1)
+    mdk_token = MDNToken.deploy(initial_supply, {"from": account_1, "gas_price": gas_strategy})
     print(mdk_token.name())
+
+    ## Testing transfering tokens
+    transfer_to_account = get_account(index=2)
+    transfer = mdk_token.transfer(transfer_to_account, 99, {'from': account_1, 'gas_price': gas_strategy})
+
+    ## Check if account 2 have the tokens
